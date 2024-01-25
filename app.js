aff21e10ebc1644067e8e2bf418db98b405cc851
NilimaSonawane519 <152363473+NilimaSonawane519@users.noreply.github.com>
2024-01-25 10:57:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const item=document.querySelector("#item");
+const todobox=document.querySelector("#to-do-box");
+
+item.addEventListener(
+    "keyup",function(event){
+        if(event.key === "Enter")
+        {
+            addToDo(this.value);
+            this.value ="";
+        }
+
+    }
+);
+const addToDo = (item)=>{
+    const listitem = document.createElement("li");
+    listitem.innerHTML=`${item}
+
+    <i class="fas fa-times"></i>`;
+    listitem.addEventListener("click",
+    function(event){
+        this.classList.toggle("done");
+    }
+    );
+    listitem.querySelector("i").addEventListener(
+        "click",function(){
+            listitem.remove();
+        }
+    );
+    todobox.appendChild(listitem);
+}
+
+
